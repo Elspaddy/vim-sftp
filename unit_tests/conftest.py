fe4531a3df5d2da3840de7cdf8f76d5052e67252
Alan M Silva <alanfortlink@gmail.com>
2019-11-15 19:44:11 +0000

    add test for __eq__ on SFTPConfig

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -1,13 +1,25 @@
 import pytest
 
 @pytest.fixture
-def valid_config_file():
+def valid_config_file1():
     content = {
         "user": "foo",
         "host": "mock.host.com",
         "port": 2222,
         "key_path": "./unit_tests/configs/valid_key",
         "path": "./unit_tests/configs/successful_configs.json"
+    }
+
+    yield content
+
+@pytest.fixture
+def valid_config_file2():
+    content = {
+        "user": "bar",
+        "host": "mock2.host.com",
+        "port": 2222,
+        "key_path": "./unit_tests/configs/valid_key",
+        "path": "./unit_tests/configs/successful_configs2.json"
     }
 
     yield content
--- unit_tests/test_sftp_configs.py
+++ unit_tests/test_sftp_configs.py
@@ -18,11 +18,23 @@ def test_failing_construction(config_file):
     with pytest.raises(Exception):
         SFTPConfig(config_file)
 
-def test_parsing_correct(valid_config_file):
-    sftp_config = SFTPConfig(valid_config_file['path'])
+def test_parsing_correct(valid_config_file1):
+    sftp_config = SFTPConfig(valid_config_file1['path'])
     assert sftp_config
 
-    assert sftp_config.user() == valid_config_file['user']
-    assert sftp_config.host() == valid_config_file['host']
-    assert sftp_config.port() == valid_config_file['port']
-    assert sftp_config.key_path() == valid_config_file['key_path']
+    assert sftp_config.user() == valid_config_file1['user']
+    assert sftp_config.host() == valid_config_file1['host']
+    assert sftp_config.port() == valid_config_file1['port']
+    assert sftp_config.key_path() == valid_config_file1['key_path']
+
+def test_eq_function(valid_config_file1):
+    sftp_config1 = SFTPConfig(valid_config_file1['path'])
+    sftp_config2 = SFTPConfig(valid_config_file1['path'])
+
+    assert sftp_config1 == sftp_config2
+
+def test_not_eq_function(valid_config_file1, valid_config_file2):
+    sftp_config1 = SFTPConfig(valid_config_file1['path'])
+    sftp_config2 = SFTPConfig(valid_config_file2['path'])
+
+    assert not sftp_config1 == sftp_config2
